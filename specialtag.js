b155245bf581d5ee10580d5d31412482e5fc82cd
Thivi <theviyanthan@outlook.com>
2019-06-17 11:02:40 +0530

    icon changed

--- specialtag.js
+++ specialtag.js
@@ -2,7 +2,7 @@
     var MyCustomButton = function( props ) {
         return wp.element.createElement(
             wp.editor.RichTextToolbarButton, {
-                icon: 'image-flip-horizontal',
+                icon: 'image-rotate',
                 title: 'Special tag',
                 onClick: function() {
                     props.onChange( wp.richText.toggleFormat(
